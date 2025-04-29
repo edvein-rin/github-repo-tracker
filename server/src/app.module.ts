dc4180936f30c0fbab27b85a01dc96fae496a0e2
Eduard Tsakhlo <edveinrin@gmail.com>
2025-04-30 01:31:15 +0300

    feat: setup `TypeORM`

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { GithubModule } from './github/github.module';
 import { UserModule } from './user/user.module';
 import { RepositoryModule } from './repository/repository.module';
 import { UserRepositoryModule } from './user-repository/user-repository.module';
+import { ConfigService } from './config/config.service';
 import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
     ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
     GithubModule,
     RepositoryModule,
     UserModule,
