456a8b63017fa237ad355b93ba65568f354f79c2
Eduard Tsakhlo <edveinrin@gmail.com>
2025-04-30 18:02:37 +0300

    fix: provide user in JWT validate function

--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -3,10 +3,15 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
 import { ConfigService } from 'src/config/config.service';
+import { UserWithoutCredentials } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(
+    private userService: UserService,
+    configService: ConfigService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,7 +19,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: { sub: string; username: string }) {
-    return { userId: payload.sub, username: payload.username };
+  async validate(payload: {
+    sub: number;
+    username: string;
+  }): Promise<UserWithoutCredentials> {
+    const userId = payload.sub;
+    const user = await this.userService.findOne(userId);
+    if (!user) {
+      throw new Error('Impossible case');
+    }
+
+    return user;
   }
 }
